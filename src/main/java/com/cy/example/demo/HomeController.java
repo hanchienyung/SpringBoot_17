8615a85ad1e01821fb658ef7cdb4a89436355593
hanchienyung <hanchienyung@Yahoo.com>
2018-02-12 19:37:25 -0500

    Including Lesson 21

--- src/main/java/com/cy/example/demo/HomeController.java
+++ src/main/java/com/cy/example/demo/HomeController.java
@@ -1,21 +1,40 @@
 package com.cy.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
 
 @Controller
 public class HomeController {
+
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/register")
+    public String showRegistrationPage(Model model){
+       return "registration";
+    }
+
     @RequestMapping("/")
     public String index(){
         return "index";
     }
+
     @RequestMapping("/login")
     public String login(){
         return "login";
     }
+
     @RequestMapping("/secure")
     public String secure(){
         return "secure";
     }
 
+    @RequestMapping("/admin")
+    public String admin(){ return "admin"; }
 }
